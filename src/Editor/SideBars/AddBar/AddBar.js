9c74cc2eb7743d0d554e8e80f7f4279acd7af5b0
Titouan Baillon <titouan.baillon@gmail.com>
2018-12-12 15:38:50 +0100

    display node description

--- src/Editor/SideBars/AddBar/AddBar.js
+++ src/Editor/SideBars/AddBar/AddBar.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
 import ColorCycler from '../../../ColorCycler';
+import { toast } from 'react-toastify';
 import './AddBar.css';
 
 class AddBar extends Component {
   constructor(props){
     super(props);
+    this.__toastDescription = this.__toastDescription.bind(this);
     this.__renderNode = this.__renderNode.bind(this);
     this.__addNode = this.__addNode.bind(this);
     this.__dragNode = this.__dragNode.bind(this);
     this.__cc = new ColorCycler();
+  }
+  __toastDescription(node){
+    const n = new node();
+    toast.info(n.editor.description, {
+      position: "bottom-center",
+      autoClose: 5000,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: false,
+      draggable: true,
+      closeButton: false
+    });
   }
   __renderNode(c,node){
     const ctx = c.getContext("2d");
@@ -37,7 +51,7 @@ class AddBar extends Component {
     const dom = [];
     if(this.props.mesh){
       for(let [name,node] of this.props.mesh.constructor.nodeConstructors){
-        dom.push(<canvas ref={name} onClick={e=>this.__addNode(e.target,node)} onDragStart={e=>this.__dragNode(e,e.target,node)} draggable="true" width="160" height="160" className="addbar-canvas"/>);
+        dom.push(<canvas ref={name}  onClick={e=>this.__addNode(e.target,node)} onContextMenu={e=>{this.__toastDescription(node);e.preventDefault();}} onDragStart={e=>this.__dragNode(e,e.target,node)} draggable="true" width="160" height="160" className="addbar-canvas"/>);
       }
     }
     return <div className="addbar-box">{dom}</div>;
