9c9de32d0d0cd2a7c8917f839342b207e6fb8819
Titouan Baillon <titouan.baillon@gmail.com>
2018-12-17 23:27:31 +0100

    added add and remove methods

--- src/MeshScript/Core/MeshScript.js
+++ src/MeshScript/Core/MeshScript.js
@@ -1,5 +1,7 @@
 import IterableObject from '../../utils/IterableObject';
+import Random from '../../utils/Random';
 import {Link} from "./Link";
+import {Node} from "./Node";
 export class MeshScript{
   constructor(descriptor = {}){
     this.nodes=new IterableObject();
@@ -11,16 +13,50 @@ export class MeshScript{
     const nodeDescriptors = new IterableObject(descriptor.nodes||{});
     const linkDescriptors = new IterableObject(descriptor.links||{});
     //node creation from patern
-    for(const [id,n] of nodeDescriptors){
-      if(n.type && n.type in MeshScript.nodeConstructors){
-        this.nodes[id]=new MeshScript.nodeConstructors[n.type](n);
+    for(const [id,n] of nodeDescriptors)this.addNode(n,id);
+    //link creation from patern
+    for(const [id,l] of linkDescriptors)this.addLink(l,id);
+  }
+  addNode(n,id = Random.string){
+    if(typeof n == "string") n = JSON.parse(n);
+    if(n.type && n.type in MeshScript.nodeConstructors){
+      this.nodes[id]=new MeshScript.nodeConstructors[n.type](n);
+      return id;
+    }else{
+      throw new Error(`Couldn't create Node: ${n.type} not in nodeConstructors`);
+    }
+    return id;
+  }
+  addLink(l,id = Random.string){
+    if(typeof l == "string") l = JSON.parse(l);
+    if(l.start && l.end && l.output){
+      if(this.nodes[l.start]) l.start = this.nodes[l.start];
+      if(this.nodes[l.end]) l.end = this.nodes[l.end];
+      if( l.start instanceof Node &&  l.end instanceof Node){
+        this.links[id]=new Link(l.start,l.output,l.end,l);
+      }else{
+        throw new Error(`Couldn't create Link: node(s) unknown`);
+      }
+    }else{
+      throw new Error(`Couldn't create Link: parameter missing`);
+    }
+    return id;
+  }
+  removeNode(node){
+    for(const [id,n] of this.nodes){
+      if(node == n)delete this.nodes[id];
+    }
+    for(const [id,link] of this.links){
+      if(link.start == node || link.end == node){
+        delete this.links[id];
+        link.disconnect();
       }
     }
-    //link creation from patern
-    for(const [id,l] of linkDescriptors){
-      if(l.start && l.end && l.output){
-        this.links[id]=new Link(this.nodes[l.start],l.output,this.nodes[l.end],l);
-      }
+  }
+  removeLink(link){
+    link.disconnect();
+    for(const [id,l] of this.links){
+      if(link == l)delete this.links[id];
     }
   }
   destroy(){
